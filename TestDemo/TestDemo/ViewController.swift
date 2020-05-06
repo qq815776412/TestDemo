b0cbfc5821589b3ecbab61a78d56c63e77d55e65
gjm <815776412@qq.com>
2020-05-06 20:06:26 +0800

    ss

--- TestDemo/TestDemo/ViewController.swift
+++ TestDemo/TestDemo/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 2, green: 2, blue: 3, alpha: 1)
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
         // Do any additional setup after loading the view.
     }
 
