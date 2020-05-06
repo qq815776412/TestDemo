c95cb6acc43a0f70124bcadf83cba8c674a0b25d
gjm <815776412@qq.com>
2020-05-06 13:22:34 +0800

    a

---
+++ TestDemo/TestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestDemo
+//
+//  Created by gjm on 2020/5/6.
+//  Copyright © 2020 gjm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestDemo/TestDemoTests/TestDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestDemoTests.swift
+//  TestDemoTests
+//
+//  Created by gjm on 2020/5/6.
+//  Copyright © 2020 gjm. All rights reserved.
+//
+
+import XCTest
+@testable import TestDemo
+
+class TestDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
